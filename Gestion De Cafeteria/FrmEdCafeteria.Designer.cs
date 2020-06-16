75a67fe74744ac62a5e0ae06229095df8e341653
danamariacruz <danacruzpaulini@gmail.com>
2020-06-16 19:56:54 -0400

    Crud-cafeteria

---
+++ Gestion De Cafeteria/FrmEdCafeteria.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Gestion_De_Cafeteria
+{
+    partial class FrmEdCafeteria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CmbEstado = new System.Windows.Forms.ComboBox();
+            this.CmbEncargado = new System.Windows.Forms.ComboBox();
+            this.CmbCampus = new System.Windows.Forms.ComboBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CmdEliminar = new System.Windows.Forms.Button();
+            this.CmdGuardar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.CmbEstado);
+            this.panel1.Controls.Add(this.CmbEncargado);
+            this.panel1.Controls.Add(this.CmbCampus);
+            this.panel1.Controls.Add(this.txtDescripcion);
+            this.panel1.Controls.Add(this.txtID);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(328, 245);
+            this.panel1.TabIndex = 0;
+            // 
+            // CmbEstado
+            // 
+            this.CmbEstado.FormattingEnabled = true;
+            this.CmbEstado.Items.AddRange(new object[] {
+            "Activo",
+            "Inactivo"});
+            this.CmbEstado.Location = new System.Drawing.Point(96, 181);
+            this.CmbEstado.Name = "CmbEstado";
+            this.CmbEstado.Size = new System.Drawing.Size(121, 21);
+            this.CmbEstado.TabIndex = 9;
+            // 
+            // CmbEncargado
+            // 
+            this.CmbEncargado.FormattingEnabled = true;
+            this.CmbEncargado.Location = new System.Drawing.Point(96, 142);
+            this.CmbEncargado.Name = "CmbEncargado";
+            this.CmbEncargado.Size = new System.Drawing.Size(121, 21);
+            this.CmbEncargado.TabIndex = 8;
+            // 
+            // CmbCampus
+            // 
+            this.CmbCampus.FormattingEnabled = true;
+            this.CmbCampus.Location = new System.Drawing.Point(96, 100);
+            this.CmbCampus.Name = "CmbCampus";
+            this.CmbCampus.Size = new System.Drawing.Size(121, 21);
+            this.CmbCampus.TabIndex = 7;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(96, 63);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(121, 20);
+            this.txtDescripcion.TabIndex = 6;
+            // 
+            // txtID
+            // 
+            this.txtID.Enabled = false;
+            this.txtID.Location = new System.Drawing.Point(96, 27);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(121, 20);
+            this.txtID.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Estado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Encargado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Campus";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Descripcion";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // CmdEliminar
+            // 
+            this.CmdEliminar.Location = new System.Drawing.Point(184, 274);
+            this.CmdEliminar.Name = "CmdEliminar";
+            this.CmdEliminar.Size = new System.Drawing.Size(75, 54);
+            this.CmdEliminar.TabIndex = 5;
+            this.CmdEliminar.Text = "Eliminar";
+            this.CmdEliminar.UseVisualStyleBackColor = true;
+            this.CmdEliminar.Click += new System.EventHandler(this.CmdEliminar_Click);
+            // 
+            // CmdGuardar
+            // 
+            this.CmdGuardar.Location = new System.Drawing.Point(77, 274);
+            this.CmdGuardar.Name = "CmdGuardar";
+            this.CmdGuardar.Size = new System.Drawing.Size(75, 54);
+            this.CmdGuardar.TabIndex = 4;
+            this.CmdGuardar.Text = "Guardar";
+            this.CmdGuardar.UseVisualStyleBackColor = true;
+            this.CmdGuardar.Click += new System.EventHandler(this.CmdGuardar_Click);
+            // 
+            // FrmEdCafeteria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 340);
+            this.Controls.Add(this.CmdEliminar);
+            this.Controls.Add(this.CmdGuardar);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmEdCafeteria";
+            this.Text = "Edicion cafeteria";
+            this.Load += new System.EventHandler(this.FrmEdCafeteria_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox CmbEstado;
+        private System.Windows.Forms.ComboBox CmbEncargado;
+        private System.Windows.Forms.ComboBox CmbCampus;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CmdEliminar;
+        private System.Windows.Forms.Button CmdGuardar;
+    }
+}
