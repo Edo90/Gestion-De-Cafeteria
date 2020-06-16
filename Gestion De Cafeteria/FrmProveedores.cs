ef03aca9995815227847e21360fcbd6c228c78d6
Nehemias Feliz <juniornff@gmail.com>
2020-06-16 01:10:20 -0400

    Furmularios de proveedores añadido, aunque aun le faltan las entidades

---
+++ Gestion De Cafeteria/FrmProveedores.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_De_Cafeteria
+{
+    public partial class FrmProveedores : Form
+    {
+        public Proveedores proveedor { get; set; }
+        private Entities entities = new Entities();
+        public FrmProveedores()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmProveedores_Load(object sender, EventArgs e)
+        {
+            ConsultarProveedores();
+        }
+
+        private void CmdBuscar_Click(object sender, EventArgs e)
+        {
+            ConsultarPorCriterio();
+        }
+
+        private void CmdAgregar_Click(object sender, EventArgs e)
+        {
+            FrmProveedoresEd fpe = new FrmProveedoresEd();
+            fpe.ShowDialog();
+        }
+
+        private void FrmProveedores_Activated(object sender, EventArgs e)
+        {
+            ConsultarProveedores();
+        }
+
+        private void DgvProveedores_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = this.DgvProveedores.SelectedRows[0];
+            Proveedores proveedor = new Proveedores();
+            proveedor.IdProveedor = Int32.Parse(row.Cells[0].Value.ToString());
+            proveedor.NombreComercial = row.Cells[1].Value.ToString();
+            proveedor.RNC = row.Cells[2].Value.ToString();
+            proveedor.FechaRegistro = row.Cells[3].Value.ToString();
+            proveedor.Estado = row.Cells[4].Value.ToString();
+            FrmProveedoresEd fpe = new FrmProveedoresEd();
+            fpe.proveedor = proveedor;
+            fpe.ShowDialog();
+        }
+
+        private void ConsultarProveedores()
+        {
+            DgvProveedores.DataSource = entities.Proveedores.ToList();
+        }
+
+        private void ConsultarPorCriterio()
+        {
+            var proveedores = from em in entities.Proveedores
+                              where (em.IdProveedor.ToString().StartsWith(TxtDatoABuscar.Text) ||
+                            em.NombreComercial.StartsWith(TxtDatoABuscar.Text) ||
+                            em.RNC.StartsWith(TxtDatoABuscar.Text) ||
+                            em.FechaRegistro.ToString().StartsWith(TxtDatoABuscar.Text) ||
+                            em.Estado.ToString().StartsWith(TxtDatoABuscar.Text)
+                            )
+                              select em;
+            DgvProveedores.DataSource = proveedores.ToList();
+        }
+    }
+}
---
+++ Gestion De Cafeteria/FrmProveedoresEd.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_De_Cafeteria
+{
+    public partial class FrmProveedoresEd : Form
+    {
+        public Proveedores proveedor { get; set; }
+        private Entities entities = new Entities();
+        public FrmProveedoresEd()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmProveedoresEd_Load(object sender, EventArgs e)
+        {
+            CbxEstado.SelectedIndex = 0;
+            if (proveedor != null)
+            {
+                TxtIdProveedor.Text = proveedor.IdEmpleado.ToString();
+                TxtNombreComercial.Text = proveedor.NombreComercial;
+                TxtRNC.Text = proveedor.RNC.ToString();
+                TxtFechaRegistro.Text = proveedor.FechaRegistro.ToString();
+                if (proveedor.Estado == "Inactivo")
+                {
+                    CbxEstado.SelectedIndex = 1;
+                }
+            }
+        }
+
+        private void CmdGuadar_Click(object sender, EventArgs e)
+        {
+            Proveedores proveedor = entities.Proveedores.Find(Int32.Parse(TxtIdProveedor.Text));
+
+            if (proveedor != null)
+            {
+                proveedor.NombreComercial = TxtNombreComercial.Text;
+                proveedor.RNC = TxtRNC.Text;
+                proveedor.FechaRegistro = TxtFechaRegistro.Text;
+                proveedor.Estado = CbxEstado.Text;
+            }
+            else
+            {
+                entities.Proveedores.Add(new Proveedores
+                {
+                    NombreComercial = TxtNombreComercial.Text,
+                    RNC = TxtRNC.Text,
+                    FechaRegistro = TxtFechaRegistro.Text,
+                    Estado = CbxEstado.Text
+                });
+            }
+            entities.SaveChanges();
+            MessageBox.Show("Datos guardados con exito");
+            this.Close();
+        }
+
+        private void CmdEliminar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Seguro de eliminar?", "Alerta", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                return;
+            }
+
+            Proveedores proveedor = entities.Proveedores.Find(Int32.Parse(TxtIdProveedor.Text));
+            if (proveedor != null)
+            {
+                entities.Proveedores.Remove(proveedor);
+                entities.SaveChanges();
+                MessageBox.Show("Empleado eliminado con exito");
+            }
+            else
+            {
+                MessageBox.Show("Empleado no existe");
+            }
+            this.Close();
+        }
+    }
+}
