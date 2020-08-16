5417fbe52201583a0aeeb2f9fb001c25c2ca5978
danamariacruz <danacruzpaulini@gmail.com>
2020-08-16 19:33:04 -0400

    Reporte de ventas

---
+++ Gestion De Cafeteria/FrmReporteVentas.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Gestion_De_Cafeteria
+{
+    partial class FrmReporteVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TxtHasta = new System.Windows.Forms.DateTimePicker();
+            this.TxtDesde = new System.Windows.Forms.DateTimePicker();
+            this.BtnGenerar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.TxtHasta);
+            this.panel1.Controls.Add(this.TxtDesde);
+            this.panel1.Controls.Add(this.BtnGenerar);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(705, 238);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // TxtHasta
+            // 
+            this.TxtHasta.Location = new System.Drawing.Point(411, 60);
+            this.TxtHasta.Name = "TxtHasta";
+            this.TxtHasta.Size = new System.Drawing.Size(123, 20);
+            this.TxtHasta.TabIndex = 13;
+            // 
+            // TxtDesde
+            // 
+            this.TxtDesde.Location = new System.Drawing.Point(235, 60);
+            this.TxtDesde.Name = "TxtDesde";
+            this.TxtDesde.Size = new System.Drawing.Size(104, 20);
+            this.TxtDesde.TabIndex = 12;
+            // 
+            // BtnGenerar
+            // 
+            this.BtnGenerar.Location = new System.Drawing.Point(307, 122);
+            this.BtnGenerar.Name = "BtnGenerar";
+            this.BtnGenerar.Size = new System.Drawing.Size(109, 51);
+            this.BtnGenerar.TabIndex = 11;
+            this.BtnGenerar.Text = "Ver Excel";
+            this.BtnGenerar.UseVisualStyleBackColor = true;
+            this.BtnGenerar.Click += new System.EventHandler(this.BtnGenerar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(367, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hasta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Desde:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(269, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Reporte de ventas";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmReporteVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 344);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmReporteVentas";
+            this.Text = "FrmReporteVentas";
+            this.Load += new System.EventHandler(this.FrmReporteVentas_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtnGenerar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker TxtHasta;
+        private System.Windows.Forms.DateTimePicker TxtDesde;
+    }
+}
